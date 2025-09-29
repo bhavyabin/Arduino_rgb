c0211ffc57c83fc98fc560277af44832539c91ee
bhavyabin <bkrockxxx@gmail.com>
2025-09-29 19:48:32 +0530

    main

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/api", methods=["POST"])
+def api():
+    with open("color.txt", "w") as file:
+        file.write(request.json["color"])
+
+    print(request.json)
+    return request.json["color"]
+
+
+@app.route("/get")
+def get():
+    with open("color.txt", "r") as file:
+        return file.read()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
